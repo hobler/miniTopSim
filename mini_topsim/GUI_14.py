e0cc6f30762c79f10f09d4b6ce08940d45bb85bf
StefanKral <stefan.kral@aon.at>
2021-01-14 04:03:49 +0100

    Gui Testversion

--- mini_topsim/GUI_14.py
+++ mini_topsim/GUI_14.py
@@ -1,4 +1,4 @@
-from gui import *
+from work.Aufgabe14_gui.gui_13 import *
 import tkinter as tk
 import tkinter.ttk as ttk
 import sys
@@ -22,7 +22,7 @@ gui_classes = []
 
 
 class GuiNew(Gui):
-    def __init__(self,maintk,frame,section,parameter_file,button_file,cfg_file):
+    def __init__(self,maintk,frame,section,parameter_file,button_file,cfg_file,initial_file):
         print("GuiNew")
         self.root = frame
         self.maintk = maintk
@@ -39,7 +39,25 @@ class GuiNew(Gui):
         self.button_file = button_file
         self.cfg_file = cfg_file
         self.data = self.get_data()
+        if initial_file is not None:
+            print(initial_file)
+            cp = configparser.ConfigParser()
+            cp.optionxform = str
+            cp.read(initial_file)
+            open(initial_file, 'r')
+            print(str(cp))
+            print("INITS:")
+            print(cp.sections())
+            for key in cp[self.section]:
+                if (cp[self.section][key]).replace(".", "").replace("-","").replace("True","0").replace("False", "0").isnumeric():
+                    val = eval(cp[self.section][key])
+                else:
+                    val = cp[self.section][key]
+                print(self.data[key])
+                self.data[key][0] = val
+                print(key + " | " + str(val))
 
+            print("----------------------")
         self.root.columnconfigure(0, weight=0)
         self.root.columnconfigure(1, weight=1)
         self.root.columnconfigure(2, weight=0)
@@ -73,10 +91,8 @@ class GuiNew(Gui):
         equal = self.data_already_in_file()
         answer = False  
         success = True
-        if not equal:
-            messagebox.showwarning('Unsaved Data', 'The data isn\'t equal to '\
-                                   'the file beispiel.cfg')
-            answer = messagebox.askyesno('Create Config file?', 'Do you want '\
+
+        answer = messagebox.askyesno('Create Config file?', 'Do you want '\
                                          'to save the parameters?')
                 
         if answer:
@@ -112,15 +128,27 @@ class GuiNew(Gui):
             is_invalid |= (not c.check_data())
             print(is_invalid)
         if not is_invalid:
-            print("KKKKKKKKKKKKKK")
-            all_data = {}
+            print("Saving")
+            cp = configparser.ConfigParser()
+            cp.optionxform = str
+            file_name = filedialog.asksaveasfilename(initialdir=self.cfg_file,
+                                                     title='Save config', filetypes=(('cfg-files', '*.cfg'),
+                                                                                     ('all files', '*.*')),
+                                                     defaultextension='.cfg',
+                                                     initialfile='beispiel2.cfg')
             for guis in gui_classes:
-                all_data.update(guis.data)
-            config = CreateConfigFile(all_data)
-            success = config.save_file(tabnames, self.cfg_file)
-            if success:
+                cp.add_section(guis.section)
+                for key, value in guis.data.items():
+                    print(value)
+                    cp.set(guis.section, key, str(value[0]))
+            if file_name:
+                with open(file_name, 'w') as file:
+                    cp.write(file)
                 messagebox.showinfo('Save', 'Save of config successfully!')
                 return True
+            else:
+                return False
+
             
 
 def test(event):
@@ -128,12 +156,21 @@ def test(event):
     i = tabs_frame.index(tabs_frame.select())
     print(i)
     gui_classes[i].onTabSwitch()
-        
 
+current_dir = os.path.dirname(os.path.abspath(__file__))
+path_to_parameter_file = os.path.join(current_dir, 'parameters.db')
+path_to_button_file = os.path.join(current_dir, 'info.png')
+parent_dir = os.path.dirname(current_dir)
+path_to_cfg_file = os.path.join(parent_dir, 'work', 'Aufgabe14_gui')
+path_to_init_file = None
+if(len(sys.argv) == 2):
+    print(sys.argv)
+    print(current_dir)
+    path_to_init_file = os.path.join(parent_dir,sys.argv[1])
 for i in range(6):
     tabs.append(ttk.Frame(tabs_frame))
     tabs_frame.add(tabs[i], text=tabnames[i])
-    gui_classes.append(GuiNew(form, tabs[i], tabnames[i], "../../../../parameters.db", "info.png", "beispiel.cfg"))
+    gui_classes.append(GuiNew(form, tabs[i], tabnames[i], path_to_parameter_file, path_to_button_file, path_to_cfg_file,path_to_init_file))
 
 #a = GuiNew(form,tabs[0],tabnames[0],"parameters.db","info.png","beispiel.cfg")
 #a.start()
