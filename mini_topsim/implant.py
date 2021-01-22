2fa27caed9d0d5a35b169a0bb344a6766d60343c
Omerasevic Armin <voodoo_xa93@hotmail.com>
2021-01-22 06:42:53 +0100

    Aufgabe16_implant: works fine but slow

--- mini_topsim/implant.py
+++ mini_topsim/implant.py
@@ -6,9 +6,7 @@ Classes:
 		Gauss: provides a callable object for calculating the Gaussian function.
 
 Functions:
-
-	__init__(self): Initializes all parameters of the class Gauss.
-	__call__(self): Calculates the Gaussian function.
+		__call__(self): Calculates the Gaussian function.
 
 Additionally this module can be used as a script:
 	USAGE: $ python3 implant.py [name of .cfg file]
@@ -17,56 +15,51 @@ Additionally this module can be used as a script:
 Author: Omerasevic Armin (01325962)
 Part of the miniTopSim Project: https://github.com/hobler/miniTopSim 
 '''
-import sys
-import matplotlib.pyplot as plt
-import numpy as np
-import mini_topsim.parameters as par
 from init_surface import init_surface
 from bulk import Bulk
+from surface import Surface
+
+import sys
+import numpy as np
+import parameters as par
+from scipy.interpolate import interp1d
 
 
 class Gauss:
 	'''
 	Provides a callable object for calculating the Gaussian function.
-	'''
-	def __init__(self):
-		'''
-		Initializes all class parameters.
-		'''
-		self.x_grid = np.arange(par.XMIN, par.XMAX + 1, par.BULK_DX)
-		self.y_surface = init_surface(self.x_grid)
-		gridline_min_surface = int(np.amin(self.y_surface)/par.BULK_DY - 1) * par.BULK_DY
-		self.y_grid = np.arange(gridline_min_surface - par.RP - 3*par.DRP, 0 + 1, par.BULK_DY)		
+	'''			
 		
-	def __call__(self):	
+	def __call__(self, bulk):	
 		'''
 		Calculates the Gaussian function.
 		We use it since the probability distribution for the end point of the ion trajectories 
-		can be approximately described by a Gaussian function.
-		
-		Returns: nodes of a regular lattice for which the dopant concentration should be calculated and
-		the dopant concentration for every node.
-			
+		can be approximately described by a Gaussian function.			
 		'''
-		surface_dict = dict(zip(self.x_grid, self.y_surface))
+		f_interp = interp1d(bulk.xvals, bulk.yvals, kind = 'cubic',bounds_error=False) # bounds_error=False: A value in x_new is below the interpolation range. Problem u V-Shape
+		#print(f_interp(-50))
+		
 		prefactor = par.DOSE*(1e-14)/(2*np.pi*par.DRP*par.DRLAT)
 		Delta_R_p_pow = 2*par.DRP**2
-		Delta_R_lat_pow = 2*par.DRLAT**2
-		
-		conc = np.zeros(shape=(len(self.x_grid),len(self.y_grid)))
-		for i, x_koor in enumerate(self.x_grid):  
-			for j, y_koor in enumerate(self.y_grid):
-				conc[i][j] = 0
-				if y_koor < surface_dict[x_koor]:
-					for psi_i in np.arange(x_koor - 3*par.DRLAT, x_koor + 3*par.DRLAT + 1, par.BULK_DX):  #Fehler in der Angabe?
+		Delta_R_lat_pow = 2*par.DRLAT**2				
+			
+		conc = np.zeros(shape=(len(bulk.x_grid),len(bulk.y_grid)))
+		for i, x_koor in enumerate(bulk.x_grid):  
+			for j, y_koor in enumerate(bulk.y_grid):
+				conc[i][j]=0.0			
+				if y_koor <= f_interp(x_koor):  #inside material
+					for psi_i in np.arange(x_koor - 3*par.DRLAT, x_koor + 3*par.DRLAT , par.BULK_DX): 
 						if psi_i >= par.XMIN and psi_i <= par.XMAX:                     
-							conc[i][j]+=np.exp(((-(y_koor-surface_dict[psi_i]-par.RP) ** 2)/Delta_R_p_pow)- \
-							(((x_koor-psi_i) ** 2)/Delta_R_lat_pow))*par.BULK_DX
-				conc[i][j]*=prefactor*100
-				
-		conc = np.transpose(conc)	
-		return self.x_grid, self.y_grid, conc
-
+							#conc[i][j]+=np.exp(-(((y_koor-f_interp(psi_i)-par.RP) ** 2)/Delta_R_p_pow)- \
+							#(((x_koor-psi_i) ** 2)/Delta_R_lat_pow))*par.BULK_DX
+							conc[i][j]+=np.exp(-(((np.absolute(np.absolute(y_koor)-np.absolute(f_interp(psi_i))-par.RP)) ** 2)/Delta_R_p_pow)- \
+							((np.absolute(x_koor-psi_i) ** 2)/Delta_R_lat_pow))*par.BULK_DX
+					conc[i][j]*=prefactor
+				#print(x_koor , y_koor, conc[i][j])
+	
+		bulk.conc = conc			
+		
+		
 if __name__ == '__main__':
 	'''
 	This module can be used as a script to plot dopant concentration on the nodes of a regular lattice,
@@ -78,11 +71,12 @@ if __name__ == '__main__':
 		config_file = sys.argv[1]
 		if config_file.endswith(".cfg"):
 			par.load_parameters(config_file)
-			gauss = Gauss()
-			x, y, conc = gauss()
-			bulk_obj = Bulk(x, y, conc)
-			bulk_obj.write_bulk()
-			bulk_obj.plot_bulk()
+			s = Surface()
+			b = Bulk(s.xvals,s.yvals)
+			g = Gauss()
+			g(b)
+			b.write_bulk()
+			b.plot_bulk()
 		else:
 			sys.exit("Wrong config file format .cfg!")
 	else:
