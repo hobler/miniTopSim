973e291ec5d06a8b064e8e2b4ae9fc8b77351d54
12104040 <e12104040@student.tuwien.ac.at>
2023-11-30 22:39:51 +0100

    Aufgabe3_plot: changed global variables to class variables and functioning selection for other keys

--- minitopsim/plot.py
+++ minitopsim/plot.py
@@ -7,7 +7,7 @@ import minitopsim.io_surface as io
 import minitopsim.surface as su
 
 
-count = 0
+
 fig, ax = plt.subplots()
 
 def plot(fname):
@@ -18,6 +18,7 @@ def plot(fname):
     mpl.rcParams['keymap.quit'] = "ä"
     mpl.rcParams['keymap.fullscreen'] = "ß"
     mpl.rcParams['keymap.yscale'] = "#"
+    mpl.rcParams["keymap.back"]="~"
 
     # Connect with with event handling
     cid = fig.canvas.mpl_connect('key_press_event',lambda event: Surface_Plotter.onkey(event,fname,srf_fobj))
@@ -26,45 +27,79 @@ def plot(fname):
 
 
 class Surface_Plotter:
+    count = 0
+    count2 = 0
+    count3 = 0
+    count4 = 0
+    d =1
+    r=1
+
     def onkey(event,fname,srf_fobj):
-        if event.key.isdigit():
+        # white space is backspace
+        if event.key==" ":
+            pass
+        elif event.key.isdigit():
             print(event.key)
-            fig.canvas.draw()
             number=int(event.key)
             srf_fobj=io.read_surface(srf_fobj,pow(2,number))
-            ax.clear()
+            if Surface_Plotter.d==1:
+                ax.clear()
             ax.plot(srf_fobj[0].x, srf_fobj[0].y, "b")
             fig.canvas.draw()
         elif event.key == 'Space':
             print("lol")
         elif event.key == 'f':
             srf_fobj=io.read_surface(srf_fobj,1)
-            ax.clear()
+            if Surface_Plotter.d==1:
+                ax.clear()
             ax.plot(srf_fobj[0].x, srf_fobj[0].y, "b")
             fig.canvas.draw()
             print(event.key)
         elif event.key == 'l':
             srf_fobj=io.read_surface(srf_fobj,10)
-            ax.clear()
+            if Surface_Plotter.d==1:
+                ax.clear()
             ax.plot(srf_fobj[0].x,srf_fobj[0].y,"b")
             fig.canvas.draw()
             print(event.key)
+        elif event.key == 'r':
+            global r
+            if Surface_Plotter.count2==0:
+                r=1
+                Surface_Plotter.count2+=1
+            elif Surface_Plotter.count2==1:
+                r=2
+                Surface_Plotter.count2=0
+            print(r)
         elif event.key == 'a':
-            global count
-            if count==0:
+            if Surface_Plotter.count==0:
                 ax.set_aspect('auto')
-                count=count+1
-            elif count==1:
+                Surface_Plotter.count+=1
+            elif Surface_Plotter.count==1:
                 ax.set_aspect('equal')
-                count=0
+                Surface_Plotter.count=0
             fig.canvas.draw()
             print(event.key)
         elif event.key == 'd':
+            if Surface_Plotter.count3==0:
+                Surface_Plotter.d=1
+                Surface_Plotter.count3+=1
+            elif Surface_Plotter.count3==1:
+                Surface_Plotter.d=2
+                Surface_Plotter.count3=0
             print(event.key)
         elif event.key == 's':
             plt.savefig(os.path.join("work/Aufgabe3_plot",fname.split(".")[0]+".png"))
             print(event.key)
         elif event.key == 'b':
+            if Surface_Plotter.count4==0:
+                ax.set_ylim(-100, 5)
+                ax.set_xlim(-150, 150)
+                Surface_Plotter.count4 += 1
+            elif Surface_Plotter.count4==1:
+                ax.autoscale()
+                Surface_Plotter.count4=0
+            fig.canvas.draw()
             print(event.key)
         elif event.key == 'q':
             plt.close()
