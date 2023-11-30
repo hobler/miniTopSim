94826b4a5b4a2d3c753f458679860e2ebb4f76a9
Nikola Szucsich <nikolaszu@gmail.com>
2023-11-30 23:51:59 +0100

    Aufgabe2_param: Add function load_parameters
    Add a function to load parameters from cfg file.
    Add test class to load parameters and save them to a file.
    Add test cases for passing tests as defined and lower case variables.

--- minitopsim/parameters.py
+++ minitopsim/parameters.py
@@ -1,12 +1,50 @@
 """Read config from file and propagate data as python variables.
 
 Configuration variables can be accessed using 'parameters.<name>'.
+
+Variables:
+    depend on content from parameters.db.
+
+Functions:
+    load_parameters(): Has to be called before variables are accessed.
+        Otherwise default values are used instead of values from the
+        .cfg file (this may lead to wrong data, e.g. when only a type
+        is specified as default value).
 """
 import os
 
 import configparser
 
 _categories = dict()
+
+
+def load_parameters(file):
+    """Load parameters from file.
+
+    Args:
+        file (string): the .cfg to load the configuration from. May be
+            relative to minitopsim directory or absolute.
+    """
+    # join directory if file is not absolute path
+    if not os.path.isabs(file):
+        file = os.path.join(os.path.dirname(__file__), file)
+
+    # read config from file
+    cfg_config = configparser.ConfigParser()
+    cfg_config.read(file)
+
+    new_values = dict()
+
+    # get all attributes defined in the cfg file
+    for section in cfg_config.sections():
+        for attribute in cfg_config[section]:
+            attribute = attribute.upper()
+            new_values[attribute] = eval(cfg_config[section].get(attribute))
+
+    for attribute, value in new_values.items():
+        if value is not None:
+            globals()[attribute] = value
+
 
 # load default config from file
 _file = os.path.join(os.path.dirname(__file__), "parameters.db")
---
+++ work/Aufgabe2_param/load_parameters.py
@@ -0,0 +1,24 @@
+import os
+import sys
+
+sys.path.insert(0, os.getcwd())
+import minitopsim.parameters as par
+
+cfg_file = sys.argv[1]
+out_file = cfg_file.replace(".cfg", ".out")
+cfg_file = os.path.join(os.path.dirname(__file__), cfg_file)
+out_file = os.path.join(os.path.dirname(__file__), out_file)
+
+try:
+    par.load_parameters(cfg_file)
+
+    file_content = ""
+    for key in dir(par):
+        if key.isupper() and not key.startswith("_"):
+            file_content = f'{file_content}{key} = {getattr(par, key)}\n'
+
+except Exception as e:
+    file_content = e.__str__()
+
+with open(out_file, "w") as f:
+    f.write(file_content)
