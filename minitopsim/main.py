1f55e34f19594d0436f53727923b2ecd4065e493
Nikola Szucsich <nikolaszu@gmail.com>
2023-12-03 10:19:37 +0100

    Aufgabe1: cleanup Code
    Cleanup code to meet pep8 codestyle guide.

--- minitopsim/main.py
+++ minitopsim/main.py
@@ -9,42 +9,37 @@ sys.path.append("..")
 from work.Aufgabe1_basic.advance import advance, timestep
 from work.Aufgabe1_basic.io_surface import init_surface, write_surface
 
+
 def minitopsim():
-   print('Running miniTopSim ...')
-   print(f'TestParameter={par.TestParameter}')
+    time = 0
+    tend = float(sys.argv[1])
+    dt = timestep(float(sys.argv[2]), time, tend)
+    filename = f'basic_{tend}_{dt}'
 
-   time = 0
-   tend = float(sys.argv[1])
-   dt = timestep(float(sys.argv[2]), time, tend)  
-   filename = f'basic_{tend}_{dt}'
+    # Initialize the surface
+    surface = init_surface()
 
-   # Initialize the surface
-   surface = init_surface()
-   
-   # Write and plot initial surface 
-   if (write_surface(surface, 0, filename + '.srf')==False):
-      exit()
-   surface.plot("Initial Surface")
+    # Write and plot initial surface
+    if not write_surface(surface, 0, filename + '.srf'):
+        exit()
+    surface.plot("Initial Surface")
 
-   # Move surface over time until tend is reached
-   while dt > 0:
-      surface = advance(surface, dt)
-      if(write_surface(surface, time + dt, filename + '.srf')==False):
-         exit()
-      time += dt
-      dt = timestep(dt, time, tend)
-      print("time = ", time, "dt = ", dt)
+    # Move surface over time until tend is reached
+    while dt > 0:
+        surface = advance(surface, dt)
+        if not write_surface(surface, time + dt, filename + '.srf'):
+            exit()
+        time += dt
+        dt = timestep(dt, time, tend)
+        print("time = ", time, "dt = ", dt)
 
-   # Plot final surface and save plot
-   surface.plot("Final Surface")
-   plt.legend()
-   plt.title("Sputtering yield simulation, tend = " + str(tend) + 
-             "s, dt = " + sys.argv[2] +"s")
+    # Plot final surface and save plot
+    surface.plot("Final Surface")
+    plt.legend()
+    plt.title("Sputtering yield simulation, tend = " + str(tend) +
+              "s, dt = " + sys.argv[2] + "s")
 
-   plt.savefig(filename + '.png')
-   plt.show()
+    plt.savefig(filename + '.png')
+    plt.show()
 
-   return True 
-
-
-
+    return True
