2583835ff2b2447fb27a60983eaa51d8a590a4be
12104040 <e12104040@student.tuwien.ac.at>
2023-12-03 15:03:40 +0100

    Aufgabe3_plot: friday comments

--- minitopsim/io_surface.py
+++ minitopsim/io_surface.py
@@ -1,3 +1,10 @@
+"""Reading srf file
+
+Functions:
+   read_surface(srf_fobj,n): reads a srf file and returns a surface object and time
+
+"""
+
 import numpy as np
 import os
 import re
@@ -7,7 +14,18 @@ import minitopsim.surface as su
 np.set_printoptions(threshold=np.inf)
 
 
+#  change srf_fobj to file object
+#  use npoints for reading
 def read_surface(srf_fobj,n=-1):
+    """Calls read_surface function to create a surface object which is then plotted
+
+        Args:
+            srf_fobj (file): the file to be plotted
+            n (int): flag indicating how file should be read
+        Returns:
+            srf_obj (Surface): surface object
+            time(float):last time of file
+    """
     filesrf = open(os.path.join("work/Aufgabe3_plot", srf_fobj), "r")
     content=filesrf.readlines()
     "c=filesrf.read()"
@@ -31,11 +49,12 @@ def read_surface(srf_fobj,n=-1):
             if "surface" not in lines and ((count % n) == 0):
                 print(count)
                 x=np.append(x,float(lines.split(" ")[0]))
+                # replace not neccessary
                 y=np.append(y,float(lines.split(" ")[1].replace("\n", "")))
             elif "surface" in lines:
                 count += 1
                 time=float(float(lines.split(" ")[1]))
-                """print("time in ns:",time)"""
+                """print("time in s:",time)"""
     elif n==-1:
         for lines in content:
             if "surface" not in lines :
@@ -46,27 +65,30 @@ def read_surface(srf_fobj,n=-1):
             elif "surface" in lines:
                 count+=1
                 time=float(float(lines.split(" ")[1]))
-                print("time in ns:",time)
+                print("time in s:",time)
     elif n == -2:
+        # points are reversed
         for lines in reversed(content):
             if "surface" not in lines:
                 x = np.append(x, float(lines.split(" ")[0]))
                 y = np.append(y, float(lines.split(" ")[1].replace("\n", "")))
             elif "surface" in lines:
                 time=float(float(lines.split(" ")[1]))
-                print("time in ns:", time)
+                print("time in s:", time)
                 break
-                """print("time in ns:",time)"""
-    elif n == -3:
+                """print("time in s:",time)"""
+    elif n <= -3:
+        z=-n-3+1
+        print(x)
         for lines in content:
             if "surface" not in lines:
                 x = np.append(x, float(lines.split(" ")[0]))
                 y = np.append(y, float(lines.split(" ")[1].replace("\n", "")))
             elif "surface" in lines:
                 count += 1
-                if (count == n + 1):
+                if (count == z + 1):
                     time = float(float(lines.split(" ")[1]))
-                    print("time in ns:", time)
+                    print("time in s:", time)
                     break
     filesrf.close()
     srf_obj=su.Surface(x,y)
--- minitopsim/plot.py
+++ minitopsim/plot.py
@@ -1,4 +1,4 @@
-"""Plotting and eventhandling
+"""Plotting and event handling
 
 Variables:
     fig and ax are for updating the plot with key press events
@@ -17,7 +17,6 @@ import matplotlib as mpl
 import os
 
 import minitopsim.io_surface as io
-import minitopsim.surface as su
 
 fig, ax = plt.subplots()
 
@@ -26,7 +25,7 @@ def plot(fname):
     """Calls read_surface function to create a surface object which is then plotted
 
         Args:
-            fname: the file to be plotted
+            fname(file): the file to be plotted
         """
     srf_fobj = fname
     srf_obj = io.read_surface(srf_fobj)
@@ -53,43 +52,51 @@ class Surface_Plotter:
         Variables: count,count2,count3,count4,d,r state variables to switch between modes
 
         Functions:
-            onkey(eventm srf_fobj)
+            onkey(eventm,srf_fobj)
         """
     count = 0
     count2 = 0
     count3 = 0
     count4 = 0
     incdec=0
-    srf_obj=su.Surface(0,0)
+    number=0
 
     d = 1
     r = 1
     k = 1
 
+    # add class method decorator (cls)
     def onkey(event, srf_fobj):
+        """Callback Function responsible for key events
+
+            Args:
+                event: controlls which
+                srf_fobj (file): file to be read
+
+        """
         # white space represents backspace
         if event.key == " ":
             if Surface_Plotter.r == 1:
 
                 if Surface_Plotter.k==0:
-                    Surface_Plotter.incdec+=1
+                    Surface_Plotter.incdec=-3-Surface_Plotter.number-1
                 elif Surface_Plotter.k==1:
-                    pass
+                    Surface_Plotter.incdec=-3-Surface_Plotter.incdec-1
                 elif Surface_Plotter.k==2:
-                    pass
+                    print("last surface already plotted")
 
             elif Surface_Plotter.r == 2:
 
                 if Surface_Plotter.k==0:
-                    Surface_Plotter.incdec -= 1
+                    Surface_Plotter.incdec=-3+Surface_Plotter.number+1
                 elif Surface_Plotter.k==1:
-                    pass
+                    print("first surface already plotted")
                 elif Surface_Plotter.k==2:
-                    pass
+                    Surface_Plotter.incdec=-3-Surface_Plotter.incdec+1
 
             if Surface_Plotter.d == 1:
                     ax.clear()
-
+            "print(Surface_Plotter.incdec)"
             srf_obj = io.read_surface(srf_fobj,Surface_Plotter.incdec)
             ax.plot(srf_obj[0].x, srf_obj[0].y, "b")
             fig.canvas.draw()
@@ -99,6 +106,7 @@ class Surface_Plotter:
             print(event.key)
             number = int(event.key)
             srf_obj = io.read_surface(srf_fobj, pow(2, number))
+            Surface_Plotter.incdec=number
             if Surface_Plotter.d == 1:
                 ax.clear()
             ax.plot(srf_obj[0].x, srf_obj[0].y, "b")
