8af35827602dc6d2a7838b5575d6ea3be995443d
Patrick Pably <patrick.pably@gmx.at>
2023-12-12 17:34:26 +0100

    Aufgabe6_unittest: clean-up and docstring
    Signed-off-by: Patrick Pably <patrick.pably@gmx.at>

--- work/Aufgabe6_unittest/test_advance.py
+++ work/Aufgabe6_unittest/test_advance.py
@@ -1,22 +1,34 @@
 """
+Test cases to test the etching algorithm (surface advancement and direction).
 
+tests:
+    surface_normal:
+        checks direction of etching
+    advance:
+        checks the progression of etching
+
+fixtures:
+    set_surface:
+        init a test surface with 3 points
+    set_advance_param:
+        sets etching progression parameters
 """
 import pytest
 import numpy as np
 
-import minitopsim.surface as srf
 import minitopsim.parameters as par
+import minitopsim.surface as srf
 import minitopsim.advance as adv
 
-
-_FLOATING_ERROR = 0.001
+# parameter to set max error for float value testing
+FLOATING_ERROR = 0.001
 
 
 @pytest.fixture
-def _set_surface():
+def set_surface():
     """
+    initiate a test surface with points (-1,0) , (0,0) und (1,2).
     """
-    surface = None
     x = np.array((-1., 0., 1.))
     y = np.array((0., 0., 2.))
     surface = srf.Surface(x, y)
@@ -24,16 +36,16 @@ def _set_surface():
 
 
 @pytest.mark.unittest
-def test_surface_normal(_set_surface):
+def test_surface_normal(set_surface):
     """
     test for the normal vector for a set of 3 points defined in _set_surface.
 
     Args:
-        _set_surface: fixture that sets the surface points
+        set_surface(fixture): init a surface
     """
     # todo improve readability
     #   get rid of arrays
-    surf = _set_surface
+    surf = set_surface
     # calc surface normal
     x_ref = np.diff(surf.x)
     y_ref = np.diff(surf.y)
@@ -54,36 +66,36 @@ def test_surface_normal(_set_surface):
     y = n_vecs[:, 1][1]
 
     # test for x in range
-    assert abs(x_ref - x) <= _FLOATING_ERROR, \
-        f'diff in x:{abs(x_ref - x)} is not in limit {_FLOATING_ERROR}'
+    assert abs(x_ref - x) <= FLOATING_ERROR, \
+        f'diff in x:{abs(x_ref - x)} is not in limit {FLOATING_ERROR}'
     # test for y in range
-    assert abs(y_ref - y) <= _FLOATING_ERROR, \
-        f'diff in y:{abs(y_ref - y)} is not in limit {_FLOATING_ERROR}'
+    assert abs(y_ref - y) <= FLOATING_ERROR, \
+        f'diff in y:{abs(y_ref - y)} is not in limit {FLOATING_ERROR}'
 
 
 @pytest.fixture
-def _set_advance_param():
+def set_advance_param():
     """
-    setting ETCH_RATE and TIME_STEP
+    setting ETCH_RATE=5 and TIME_STEP=1
     """
     par.__dict__['ETCH_RATE'] = 5
     par.__dict__['TIME_STEP'] = 1
 
 
 @pytest.mark.unittest
-def test_advance(_set_advance_param, _set_surface):
+def test_advance(set_advance_param, set_surface):
     """
-    todo: docstring test_advance
+    test etching progression
 
     Args:
-        _set_advance_param:
-        _set_surface:
+        set_advance_param(fixture): to set parameters
+        set_surface(fixture): to init a surface
     """
     # todo _test_advance: cleanup ref stuff
     # get referenz values
     d_t = par.TIME_STEP
     etch_r = par.ETCH_RATE
-    surf = _set_surface
+    surf = set_surface
     n_vecs = surf.normal_vector()
     x_ref = surf.x
     y_ref = surf.y
@@ -96,7 +108,7 @@ def test_advance(_set_advance_param, _set_surface):
     x = surf_adv.x
     y = surf_adv.y
 
-    assert np.all(abs(x_ref - x) <= _FLOATING_ERROR), \
-        f'diff in x:{abs(x_ref - x)} is not in limit {_FLOATING_ERROR}'
-    assert np.all(abs(y_ref - y) <= _FLOATING_ERROR), \
-        f'diff in y:{abs(y_ref - y)} is not in limit {_FLOATING_ERROR}'
+    assert np.all(abs(x_ref - x) <= FLOATING_ERROR), \
+        f'diff in x:{abs(x_ref - x)} is not in limit {FLOATING_ERROR}'
+    assert np.all(abs(y_ref - y) <= FLOATING_ERROR), \
+        f'diff in y:{abs(y_ref - y)} is not in limit {FLOATING_ERROR}'
--- work/Aufgabe6_unittest/test_parameters.py
+++ work/Aufgabe6_unittest/test_parameters.py
@@ -1,55 +1,56 @@
-""""
+"""
 Provides 2 parameterized test for loading parameters from *.cfg files
 
-test-cfg files will be loaded from the directory "cfg_test_files" which
-hast to be located in the same directory as this file.
-Files are split in good and bad, according to their key format
-    filename: key*.cfg
-        e.g.:good_int_statt_float.cfg; bad_str_value.cfg
+Test configuration files are loaded from the 'cfg_test_files' directory in
+the same directory as this file.
+Files are split in good and bad, according to their format 'key*.cfg'
+    key=good:
+        will be tested with test_load_good_parameters.
+    key=bad:
+        will be tested with test_load_bad_parameters.
 
 tests:
-    def test_load_good_parameters()
-        checks if values in parameters.py are correct for given good*.cfg
-        marks:
-            unittest
-            parameterized
+    load_good_parameters:
+        checks if values are correct for given good*.cfg
+        is parameterized
 
-    def test_load_bad_parameters()
+    load_bad_parameters:
         checks if exceptions are raised for bad*.cfg files
-        marks:
-            unittest
-            parameterized
+        is parameterized
+
+fixtures:
+    _set_default_values_par:
+        loads the default values form the databank
 """
 
+import configparser
 import pytest
 import os
 import glob
-import configparser
 
 import minitopsim.parameters as par
 
-# read test files
+
+# read test files and sort in good and bad
 path = os.path.join(os.path.dirname(__file__), 'cfg_test_files')
 if os.path.exists(path):
     if not os.path.isdir(path):
         raise FileNotFoundError(f'"{path}" is not a directory.')
 else:
     raise FileNotFoundError(f'Directory "{path}" does not exist.')
-
+# sort in good and bad files, and fetch names
 good_files = glob.glob(os.path.join(path, 'good*.cfg'))
 good_names = [os.path.basename(file) for file in good_files]
 bad_files = glob.glob(os.path.join(path, 'bad*.cfg'))
 bad_names = [os.path.basename(file) for file in bad_files]
 
-default_values = dict()
-
 
 @pytest.fixture()
-def _set_default_values_par():
+def set_default_values_par():
     """
-    Sets the default values from the parameters.db to the parameters namespace
+    Set default values from databank to parameters-namespace.
 
-    this is necessary to clean up residual values for testing
+    This is necessary to clean up residual values for next test case.
     """
     # construct the path to databank via parameters.py
     file = os.path.join(os.path.dirname(par._file), 'parameters.db')
@@ -57,41 +58,39 @@ def _set_default_values_par():
     def_config.read(file)
 
     categories = dict()
+    default_values = dict()
 
-    # get all attributes defined in the db file
+    # get all attributes defined in databank file
     for section in def_config.sections():
         for attribute in def_config[section]:
             attribute = attribute.upper()
             categories[attribute] = section
-            default_values[attribute] = (
-                eval(def_config[section].get(attribute)))[0]
-    # write default in parameters namespace
+            default_values[attribute] = \
+                (eval(def_config[section].get(attribute)))[0]
+
+    # write default values to parameters namespace
     for key, value in default_values.items():
         par.__dict__[key] = value
 
 
+@pytest.mark.unittest
 @pytest.mark.parametrize('cfg_file',
                          good_files,
                          ids=good_names
                          )
-@pytest.mark.unittest
-def test_load_good_parameters(_set_default_values_par, cfg_file):
+def test_load_good_parameters(set_default_values_par, cfg_file):
     """
-    check if the variables in parameters.py have the same value as in cfg_file.
+    checks if cfg-values are correctly loaded to parameters namespace
 
-    uses file names as ids
+    uses cfg-file-names as ids
 
-    fixtures:
-        _set_default_values_par(object)
-    parameters:
-        cfg_file(str):
-    marks:
-        unittest
+    Args:
+        set_default_values_par(fixture): sets the default values.
+        cfg_file(string): path to cfg-file
     """
-    # unit under test
     par.load_parameters(cfg_file)
 
-    # read cfg to get compare values
+    # get compare values from cfg-file
     cfg_config = configparser.ConfigParser()
     cfg_config.read(cfg_file)
 
@@ -101,41 +100,30 @@ def test_load_good_parameters(_set_default_values_par, cfg_file):
     for section in cfg_config.sections():
         for attribute in cfg_config[section]:
             attribute = attribute.upper()
-
-            if attribute in default_values:
-                cfg_param[attribute] = eval(
-                    cfg_config[section].get(attribute))
-            else:
-                assert False, (f'Attribute {attribute} not defined in '
-                               f'default values!')
+            cfg_param[attribute] = eval(cfg_config[section].get(attribute))
 
     # assert test for values
     for key, value in cfg_param.items():
         assert par.__dict__[key] == value, (f'{key} has a different value '
                                             f'than '
                                             f'{os.path.basename(cfg_file)} '
-                                            f'suggests')
+                                            f'suggests it should have.')
 
 
+@pytest.mark.unittest
 @pytest.mark.parametrize('cfg_file',
                          bad_files,
                          ids=bad_names)
-@pytest.mark.unittest
-def test_load_bad_parameters(_set_default_values_par, cfg_file):
+def test_load_bad_parameters(set_default_values_par, cfg_file):
     """
     checks if a bad*.cfg raises an Exception.
 
     uses file names as ids
 
-    fixtures:
-        _set_default_values_par(object)
-    parameters:
-        cfg_file(str):
-    marks:
-        unittest
+    Args:
+        set_default_values_par(fixture): sets the default values.
+        cfg_file(string): path to cfg-file
     """
-    # with pytest.raises(Exception) as excinfo:
-    #     par.load_parameters(cfg_file)
-    # print(f'\tRaised: {excinfo.typename}')
+
     with pytest.raises(Exception):
         par.load_parameters(cfg_file)
