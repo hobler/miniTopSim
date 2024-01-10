9fdb1d900aa00e5be3c2532c0f0594be239ae0af
thackl1 <hackl@acin.tuwien.ac.at>
2024-01-10 15:03:53 +0100

    Aufgabe 9 implemented

--- minitopsim/main.py
+++ minitopsim/main.py
@@ -3,13 +3,14 @@ Main script and function to run miniTopSim.
 """
 from . import parameters as par
 from minitopsim.advance import advance, timestep
-from minitopsim.io_surface import init_surface, write_surface
+from minitopsim.io_surface import write_surface
+from work.Aufgabe9_initial.init_surface import init_surface
 from minitopsim.plot import plot
 from minitopsim.surface import Shadow_Error
 from time import process_time
 
 import sys
-
+import os
 
 def minitopsim():
     par.load_parameters(sys.argv[1])
@@ -23,14 +24,15 @@ def minitopsim():
     surface = init_surface()
 
     # Write and plot initial surface
-    if not write_surface(surface, 0, filename + '.srf'):
+    if not write_surface(surface, time, filename + '.srf'):
         exit()
 
-    t_start = process_time()
+    t_start = process_time()   
 
     try:
         while dt > 0:
             surface, dt = advance(surface, dt)
+            
             time += dt
             if not write_surface(surface, time, filename + '.srf'):
                 exit()
@@ -43,8 +45,16 @@ def minitopsim():
     finally:
         t_stop = process_time()
         print("Calculation time:", t_stop - t_start, "s")
-
+        
         if par.PLOT_SURFACE:
-            plot(filename + '.srf')
+            # Check if .srf_save is in directory
+            if os.path.isfile(filename + '.srf_save'):
+                print(f".srf_save file is found and used")
+                plot(filename + '.srf', filename + '.srf_save')
+            # Check if other .srf is specified
+            elif len(sys.argv) >= 3:
+                plot(filename + '.srf', sys.argv[2])
+            else:
+                print('No .srf_save file found / Please provide a .srf file as additional argument')
 
     return True
